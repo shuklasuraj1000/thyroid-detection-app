e07ffafab25c0662f858f561bca9fccacc5f65e3
shuklasuraj1000 <shuklasuraj1000@gmail.com>
2023-01-06 14:00:52 +0530

    code updated

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from thyroid.pipeline.training_pipeline import start_training_pipeline
 from thyroid.pipeline.batch_prediction import start_batch_prediction
 
-file_path="/config/workspace/artifact/01042023__234146/data_ingestion/dataset/test.csv"
+file_path="/config/workspace/test.csv"
 print(__name__)
 if __name__=="__main__":
      try:
